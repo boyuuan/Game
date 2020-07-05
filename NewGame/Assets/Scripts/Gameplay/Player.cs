a6104673d39355be27ccb05e4603f2c1c70bceec
boyuuan <yuanbo1127@gmail.com>
2020-07-05 01:30:37 -0400

    Player Follows mouse

--- NewGame/Assets/Scripts/Gameplay/Player.cs
+++ NewGame/Assets/Scripts/Gameplay/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : Entity
 {
+	private float depth = 10.0f;
 	protected override void Die(){
 		GameManager.Instance.EndGame();
 	}
@@ -12,8 +13,14 @@ public class Player : Entity
 		if(hp <= 0) Die();
 	}
 	void Update(){
-		if(Input.GetKeyDown(KeyCode.A)){
-			TakeDamage(1);
+		if(Input.GetMouseButtonUp(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
+				Debug.Log("dkslj");
+			Debug.Log("MousePos = " + Input.mousePosition);
+			var mousePos = Input.mousePosition;
+			var wantedPos = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, depth));
+			transform.position = wantedPos;
 		}
 	}
 }
